a114191fe117bbd19c40d0bd91205276969d59cb
prokash <pprokash102@gmail.com>
2022-08-13 20:42:10 +0600

    simple counter app create with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./Components/Counter/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-5 items-center justify-center h-[100vh]">
+      <Counter></Counter>
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/Components/Counter/Counter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div className="shadow-2xl p-10 rounded-2xl">
+      <h2 className="text-4xl">Simple counter apps</h2>
+      <div className="text-3xl text-red-500 text-center">0</div>
+      <div className="flex justify-center gap-3 my-5">
+        <button className="h-10 w-36 rounded-xl cursor-pointer text-white text-xl bg-gray-500">
+          Increment
+        </button>
+        <button className="h-10 w-36 rounded-xl cursor-pointer text-white text-xl bg-red-600">
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
