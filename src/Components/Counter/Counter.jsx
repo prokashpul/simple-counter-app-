00824692a4262f8383f3cd8990761ed696e49667
prokash <pprokash102@gmail.com>
2022-08-13 20:49:05 +0600

    useState function use and change increment and decrement state

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  //  increment handler
+  const increment = () => {
+    setCount(count + 1);
+  };
+  //  increment handler
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="shadow-2xl p-10 rounded-2xl">
       <h2 className="text-4xl">Simple counter apps</h2>
-      <div className="text-3xl text-red-500 text-center">0</div>
+      <div className="text-3xl text-red-500 text-center">{count}</div>
       <div className="flex justify-center gap-3 my-5">
-        <button className="h-10 w-36 rounded-xl cursor-pointer text-white text-xl bg-gray-500">
+        <button
+          onClick={increment}
+          className="h-10 w-36 rounded-xl cursor-pointer text-white text-xl bg-gray-500"
+        >
           Increment
         </button>
-        <button className="h-10 w-36 rounded-xl cursor-pointer text-white text-xl bg-red-600">
+        <button
+          onClick={decrement}
+          className="h-10 w-36 rounded-xl cursor-pointer text-white text-xl bg-red-600"
+        >
           Decrement
         </button>
       </div>
